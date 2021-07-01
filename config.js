fba2981a4da1b0300dae33e72eeca7cf48e28355
Abner50a <soporteabner@gmail.com>
2021-06-30 20:31:15 -0500

    Prueba

---
+++ config.js
@@ -0,0 +1,30 @@
+const {
+    NODE_ENV,
+    MONGO_ATLAS_USER,
+    MONGO_ATLAS_PASSWORD,
+    MONGO_DB_NUEVA } = process.env;
+
+    console.log(NODE_ENV)
+const databases = {
+  // test : {},  //Prueba de BD- para correr la bd o para entorno experimental
+  // dev: {},  //UNa copia exacta de production
+  // staging: {},  //UNa copia exacta para hacer prueba hacer justamente antes de producción
+  // production : {}
+
+  test: {
+    //Cambios significativos en la estructura de la bd
+    db_uri: `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}@cluster0.fup77.mongodb.net/test?retryWrites=true&w=majority`,
+  },
+  //Entorno predeterminado para trabajar en el dia a dia
+  dev: {
+    db_uri: `mongodb://db:27017/${MONGO_DB_NUEVA}`
+   // db_uri: `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}@cluster0.fup77.mongodb.net/development?retryWrites=true&w=majority`,
+  },
+  //contiene los datos reales de nuestros usuarios/clientes
+  production: {
+    //db_uri: `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}@cluster0.fup77.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  }
+  
+};
+
+module.exports = databases[NODE_ENV];
---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+
+
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express(); // Aplicación de API
+const PORT = process.env.PORT || 4020;
+
+const { db_uri } = require('./config');
+
+
+
+mongoose.connect(db_uri, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('Database connected'))
+  .catch(() => console.log('Error connecting to database...'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/prueba', (req, res) => {
+  res.status(200).json({ message: 'Todo cool xs asdads x2' });
+});
+
+app.use(require('./routers/UserRouter'));
+app.use(require('./routers/ItemRouter'));
+app.use(require('./routers/TicketRouter'));
+
+app.listen(PORT, () => console.log(`It's alive on ${PORT}!`));
