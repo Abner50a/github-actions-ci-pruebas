9fa57cc6b7dc7d8a224ebdb068c2fcead33ef2c5
Abner50a <soporteabner@gmail.com>
2021-07-05 21:59:42 -0500

    Fix

--- config.js
+++ config.js
@@ -1,30 +1,29 @@
+const PORT = process.env.PORT || 4020;
 const {
     NODE_ENV,
     MONGO_ATLAS_USER,
     MONGO_ATLAS_PASSWORD,
-    MONGO_DB_NUEVA } = process.env;
+} = process.env;
 
-    console.log(NODE_ENV)
-const databases = {
-  // test : {},  //Prueba de BD- para correr la bd o para entorno experimental
-  // dev: {},  //UNa copia exacta de production
-  // staging: {},  //UNa copia exacta para hacer prueba hacer justamente antes de producción
-  // production : { ñ ñas asdasd as hol
+const environments = {
+    // Cambios significativos en la estructura de la bd
+    test: {
+        PORT,
+        db_uri: `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}@madea.j9vdi.mongodb.net/test?retryWrites=true&w=majority`
+    },
+    // Entorno predeterminado para trabajar en el día a día       
+    dev: {
+        PORT,
+        db_uri: `mongodb://db:27017/${process.env.MONGO_DB_NAME}`
+        // db_uri: `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}@madea.j9vdi.mongodb.net/development?retryWrites=true&w=majority`
+    },
+    // Contiene los datos reales de nuestros usuarios/clientes
+    production: {
+        PORT,
+        db_uri: `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}@madea.j9vdi.mongodb.net/MasterEnCode?retryWrites=true&w=majority`
+    },
+}
 
-  test: {
-    //Cambios significativos en la estructura de la bd
-    db_uri: `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}@cluster0.fup77.mongodb.net/test?retryWrites=true&w=majority`,
-  },
-  //Entorno predeterminado para trabajar en el dia a dia
-  dev: {
-   // db_uri: `mongodb://db:27017/${MONGO_DB_NUEVA}`
-    db_uri: `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}@cluster0.fup77.mongodb.net/development?retryWrites=true&w=majority`,
-  },
-  //contiene los datos reales de nuestros usuarios/clientes
-  production: {
-    db_uri: `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}@cluster0.fup77.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-  }
-  
-};
+console.log('NODE_ENV:', NODE_ENV);
 
-module.exports = databases[NODE_ENV];
+module.exports = environments[NODE_ENV]
--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 require('dotenv').config();
 
-
-
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express(); // Aplicación de API
-const PORT = process.env.PORT || 4020;
 
-const { db_uri } = require('./config');
-
-
-
+const { db_uri, PORT } = require('./config');
+console.log(db_uri)
 mongoose.connect(db_uri, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => console.log('Database connected - base de datos conectada'))
+  .then(() => console.log('Database connected'))
   .catch(() => console.log('Error connecting to database...'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'));
 
 app.get('/prueba', (req, res) => {
-  res.status(200).json({ message: 'Todo cool xs asdads x2' });
+  res.status(200).json({ message: 'Todo chidori' });
 });
 
 app.use(require('./routers/UserRouter'));
@@ -27,3 +23,5 @@ app.use(require('./routers/ItemRouter'));
 app.use(require('./routers/TicketRouter'));
 
 app.listen(PORT, () => console.log(`It's alive on ${PORT}!`));
+
+module.exports = app;
